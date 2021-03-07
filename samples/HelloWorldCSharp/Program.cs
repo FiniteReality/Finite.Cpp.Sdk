85a75ad34b9d4a4ea30fab1d88cb71f4e8f89d01
FiniteReality <digpoealternate@gmail.com>
2021-03-07 17:08:11 +0000

    Add C# sample, work around broken output copying

---
+++ samples/HelloWorldCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace HelloWorldCSharp
+{
+    public static class Program
+    {
+        public static unsafe void Main(string[] args)
+        {
+            var ptr = (IntPtr)Native.GetHelloString();
+            var message = Marshal.PtrToStringUTF8(ptr);
+
+            Console.WriteLine($"{message}\nThis is: C# application");
+        }
+    }
+
+    internal static unsafe class Native
+    {
+        [DllImport("HelloWorldLibraryC",
+            EntryPoint = "get_hello_string",
+            ExactSpelling = true,
+            CallingConvention = CallingConvention.Cdecl)]
+        public static extern byte* GetHelloString();
+    }
+}
